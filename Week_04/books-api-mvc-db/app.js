f554115d268cfe8e7e9fbb4960fdb54d211656a3
NgJoeYi <s10262850@connect.np.edu.sg>
2024-05-09 15:02:16 +0800

    End of part 4

---
+++ Week_04/books-api-mvc-db/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const sql = require("mssql");
+const dbConfig = require("./dbConfig");
+
+const app = express();
+const port = process.env.PORT || 3000; // Use environment variable or default port
+
+app.listen(port, async () =>{
+    try {
+        await sql.connect(dbConfig);
+        console.log("Database connection established successfully");
+    } catch (err) {
+        console.error("Database connection error:", err);
+        process.exit(1);
+        // ends with code 1, signifying an error
+    }
+
+    console.log(`Server listening on port ${port}`);
+});
+
+// sigint manages graceful shutdown 
+process.on("SIGINT", async () => {
+    console.log("Server is gracefully shutting down");
+    await sql.close();
+    console.log("Database connection closed");
+    process.exit(0);
+    // ends with code 0, signifying a clean shutdown
+})
